066812063ee39db06a0a8d1f1e81d1702bcb50bb
MNabeelAltaf <nabeelaltaf@outlook.com>
2024-10-09 11:09:09 +0500

    changes-in-pagination

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -47,7 +47,7 @@ class DashboardController extends Controller
             return back()->with('error', 'Category not found.');
         }
 
-        $animations = Resource::where('category_id', $categoryID)->paginate(1);
+        $animations = Resource::where('category_id', $categoryID)->paginate(12);
 
 
         return view('view_all_animations', ['animations' => $animations, 'category_name' => $categoryName]);
